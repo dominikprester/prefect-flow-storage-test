04ba221dedda7c514616d2882de6122c51f840b7
dominikprester <dominik.prester@axilis.com>
2021-11-04 13:32:42 +0100

    add logging

--- flows/hello_world.py
+++ flows/hello_world.py
@@ -1,15 +1,23 @@
+import prefect
 from prefect import task, Flow
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GitHub
 
 @task
 def return_world():
+    logger = prefect.context.get('logger')
+    logger.info('prvi task log')
     return "world"
 
 @task
 def hello(arg):
-    print(f'hello {arg}')
-    return f'hello {arg}'
+    logger = prefect.context.get('logger')
+    logger.info(f'got argument {arg}')
+    logger.warning('warning log test')
+    logger.error('error log test')
+    logger.info(f'result: hello {arg}')
+    logger.warning('warning log test')
+    logger.error('error log test')
 
 with Flow('Hello World Flow') as flow:
     w = return_world()
@@ -20,5 +28,3 @@ flow.storage = GitHub(
     repo='dominikprester/prefect-flow-storage-test',
     path='flows/hello_world.py'
 )
-
-
