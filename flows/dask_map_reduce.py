6f7e51280be9559cca97e3899a106cb70beb9127
dominikprester <dominik.prester@axilis.com>
2021-11-04 14:19:03 +0100

    add map reduce flow with dask executor

---
+++ flows/dask_map_reduce.py
@@ -0,0 +1,32 @@
+import prefect
+from prefect import task, Flow
+from prefect.executors import DaskExecutor
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GitHub
+
+@task
+def get_data():
+    return [*range(1000)]
+
+@task
+def map_fn(x):
+    return x + 1
+
+@task
+def reduce(x):
+    logger = prefect.context.get('logger')
+    result = sum(x)
+    logger.info(f'result: {result}')
+    return result
+
+with Flow('Dask Map Reduce') as flow:
+    data = get_data()
+    mapped_result = map_fn.map(data)
+    result = reduce(mapped_result)
+
+flow.run_config = KubernetesRun()
+flow.executor = DaskExecutor()
+flow.storage = GitHub(
+    repo='dominikprester/prefect-flow-storage-test',
+    path='flows/dask_map_reduce.py'
+)
