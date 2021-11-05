e4832480a4ba2dafb9f04846d8394915b676dd9e
dominikprester <dominik.prester@axilis.com>
2021-11-05 16:13:54 +0100

    extract task specific imports to flow

--- flows/container_dependency_test.py
+++ flows/container_dependency_test.py
@@ -3,6 +3,7 @@ from prefect import task, Flow
 from prefect.executors import DaskExecutor
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GitHub
+import dummy_package.dummy_module as dm
 
 from dask_kubernetes import KubeCluster, make_pod_spec
 
@@ -13,8 +14,6 @@ def get_kube_dask_cluster(image):
 
 @task
 def task1():
-    import dummy_package.dummy_module as dm
-
     logger = prefect.context.get('logger')
     var = dm.dummy_function_return_5()
     logger.info(f'value: {var}')
@@ -23,16 +22,12 @@ def task1():
 
 @task
 def task2(arg):
-    import dummy_package.dummy_module as dm
-
     logger = prefect.context.get('logger')
     var = dm.dummy_function_id(arg)
     logger.info(f'value: {var}')
 
 @task
 def task3():
-    import dummy_package.dummy_module as dm
-
     logger = prefect.context.get('logger')
     dm.dummy_raise_error()
     logger.error('will this print? If so, something went wrong')
